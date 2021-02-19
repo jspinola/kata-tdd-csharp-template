a44ffca070590bcdcc32a1f4319832ea2173370b
Jesus Spinola <jspinola@gmail.com>
2021-02-19 17:12:10 +0100

    Merge test cases

--- KataTests.cs
+++ KataTests.cs
@@ -9,33 +9,15 @@ namespace Kata.Test
         [InlineData(1, "I")]
         [InlineData(2, "II")]
         [InlineData(3, "III")]
-        public void should_return_up_to_three_in_roman(int number, string expected)
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        public void should_return_up_to_five_in_roman(int number, string expected)
         {
             var sut = new Kata();
 
             var result = sut.ToRoman(number);
 
             result.Should().Be(expected);
-        }
-
-        [Fact]
-        public void should_return_four_in_roman()
-        {
-            var sut = new Kata();
-
-            var result = sut.ToRoman(4);
-
-            result.Should().Be("IV");
-        }
-
-        [Fact]
-        public void should_return_five_in_roman()
-        {
-            var sut = new Kata();
-
-            var result = sut.ToRoman(5);
-
-            result.Should().Be("V");
         }
     }
 }
